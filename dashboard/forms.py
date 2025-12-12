f6c0c8f5356302686ede4f339c2f68156b6057fa
NikAce02 <nikeshmanandhar16@gmail.com>
2025-12-12 14:23:19 +0545

    dashboard form modified

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -19,10 +19,10 @@ class BlogPostForm(forms.ModelForm):
 class AddUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'groups', 'user_permissions')
 
 
 class EditUserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'groups', 'user_permissions')
